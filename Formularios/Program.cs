7b02b3927700c084ab6a02836aaddeb91a56eb85
Kevin-Roa04 <kevin.roa49u@std.uni.edu.ni>
2021-12-01 08:25:44 -0600

    Subida de las capas del proyecto

---
+++ Formularios/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //var builder = new ContainerBuilder();
+
+            //builder.RegisterType<ObjetoRepository>().As<IObjetoRespository>();
+            //builder.RegisterType<ObjetoService>().As<IObjetoService>();
+
+            //var container = builder.build();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Formulario(container.Resolve<IObjetoService>()));
+        }
+    }
+}
